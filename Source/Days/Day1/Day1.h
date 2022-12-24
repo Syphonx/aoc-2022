c172a3bc01f299460c302684b79b0e471d580b15
Jak <contact@jakbarnes.co.uk>
2022-12-24 00:53:57 +0000

    day 1 part 1

--- Source/Days/Day1/Day1.h
+++ Source/Days/Day1/Day1.h
@@ -13,8 +13,24 @@ public:
 
 	virtual std::string part1(std::string filename, std::vector<std::string> args) override
 	{
-		int count = 0;
-		return std::to_string(count);
+		std::vector<int> elves;
+		ReadAsInts(filename, elves);
+
+		int max = 0;
+		int total = 0;
+
+		for (const int line : elves)
+		{
+			total += line;
+			if (line == 0)
+			{
+				max = std::max(max, total);
+				elves.push_back(total);
+				total = 0;
+			}
+		}
+
+		return std::to_string(max);
 	}
 
 	virtual std::string part2(std::string filename, std::vector<std::string> args) override
